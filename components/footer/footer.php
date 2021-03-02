49911f7155f213adb83d9607e1a296bd55b34e6f
Hiram J. González <hiramjgonzalez98@gmail.com>
2021-03-01 22:01:23 -0400

    Add common components for all users

---
+++ components/footer/footer.php
@@ -0,0 +1,9 @@
+<footer class="row col-12">
+  <div id="copyright">
+    <span class="footer-block">
+      &copy; Copyright <?php echo date('Y'); ?> | Hiram J. González
+      <br>
+      Diseñado y desarrollado como material didáctico en Nueva Esparta, Venezuela.
+    </span>
+  </div>
+</footer>
